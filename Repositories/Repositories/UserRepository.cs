c3713189f97c6915a81acd7e5f440488356efe09
Duc Nguyen <ducnltdev@gmail.com>
2024-09-29 15:07:13 +0700

    fix

--- Repositories/Repositories/UserRepository.cs
+++ Repositories/Repositories/UserRepository.cs
@@ -14,7 +14,7 @@ public class UserRepository: BaseRepository<User>, IUserRepository
 
     public async Task<bool> CheckUsernameEmailExist(string username, string email)
     {
-        return await _entities.AnyAsync(x => x.Email.Equals(email) && x.Username.Equals(username) 
-                                                                   && x.Isdelete == false );
+        return await _entities.AnyAsync(x => x.Email.Equals(email) || x.Username.Equals(username) 
+                                                                   || x.Isdelete == false );
     }
 }
